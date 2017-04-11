477332311256f3ddf26bd7904c77f2e80e0a4bf4
TensaZangetsu <l.lawliet46@gmail.com>
2017-04-11 15:09:07 +0200

    Another herku update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/src'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
